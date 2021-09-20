d6847070309418128403b85028fb52c20dc9c1a8
MBSA13102002 <73106667+MBSA13102002@users.noreply.github.com>
2021-09-20 14:13:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,14 +18,13 @@ config = {
 firebase = Firebase(config)
 db = firebase.database()
 push_service = FCMNotification(api_key="AAAASNIN9qU:APA91bHwPAZyam3Uc4RZd2abDCeqj8Y8d8z-YR40EbnzaX98242piyWtuNn_WzGG1Rj4YTCid-1hJmtDT2Xle8gku65N32mMHdf5oKFPr6It5_npF7hbV7BzcUZSvEmmkhf-SOVTKQoN")
-def stream_handler(message):
-    # print(message["event"])
-    # print(message["path"])
-    result = push_service.notify_single_device(registration_id="cRsi5BbdRF-L0iIk4AMOjC:APA91bFUaTUVXmwGlSVgOm8HqLm1c64acOu55NJCR0Cyni3CTn8wXFnwb4A_yKrGUwPhJxak60KnJmPUMXpKadVRgCnefW832XkCpJozw-NvKO4oA_lOC3uj8GEDOLfzGFHtmKIz_Us9", message_title="Data Changed", message_body=message["data"])
 
-my_stream = db.child("data").stream(stream_handler)
 @app.route("/")
 def start():
+    def stream_handler(message):
+      result = push_service.notify_single_device(registration_id="cRsi5BbdRF-L0iIk4AMOjC:APA91bFUaTUVXmwGlSVgOm8HqLm1c64acOu55NJCR0Cyni3CTn8wXFnwb4A_yKrGUwPhJxak60KnJmPUMXpKadVRgCnefW832XkCpJozw-NvKO4oA_lOC3uj8GEDOLfzGFHtmKIz_Us9", message_title="Data Changed", message_body=message["data"])
+
+    my_stream = db.child("data").stream(stream_handler)
     return "Flask Server Started!!!"
 
 
