73c26b6c391f8652e3ff95f3c7718e1234e4bcd5
MBSA13102002 <73106667+MBSA13102002@users.noreply.github.com>
2021-10-05 12:26:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,17 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
-import random
-from pyfcm import FCMNotification
-push_service = FCMNotification(api_key="AAAASNIN9qU:APA91bHwPAZyam3Uc4RZd2abDCeqj8Y8d8z-YR40EbnzaX98242piyWtuNn_WzGG1Rj4YTCid-1hJmtDT2Xle8gku65N32mMHdf5oKFPr6It5_npF7hbV7BzcUZSvEmmkhf-SOVTKQoN")
-from firebase import Firebase
-config = {
-    "apiKey": "AIzaSyBQUpIYvs0WKfP5IMD4TE2IWTvpb5U34Cc",
-   "authDomain": "portfoliomanagement-16f09.firebaseapp.com",
-    "databaseURL": "https://portfoliomanagement-16f09.firebaseio.com",
-    "projectId": "portfoliomanagement-16f09",
-    "storageBucket": "portfoliomanagement-16f09.appspot.com",
-    "messagingSenderId": "505793158040",
-    "appId": "1:505793158040:web:14b9466a349235ef8b69ed",
-    "measurementId": "G-MRCWJ4R5RJ"
-}
-firebase = Firebase(config)
-db = firebase.database()
-auth = firebase.auth()
-user = auth.sign_in_with_email_and_password("mbsa@gmail.com", "reticulated")
+from flask import Flask ,render_template
+from flask_socketio import SocketIO, send,emit
+
+
 app = Flask(__name__)
-socketio = SocketIO(app)
-def stream_handler(message):
-    __name = db.child("stream").child("name").get().val()
-    socketio.emit('price update',__name, broadcast=True)
-    result = push_service.notify_single_device(registration_id="cRsi5BbdRF-L0iIk4AMOjC:APA91bFUaTUVXmwGlSVgOm8HqLm1c64acOu55NJCR0Cyni3CTn8wXFnwb4A_yKrGUwPhJxak60KnJmPUMXpKadVRgCnefW832XkCpJozw-NvKO4oA_lOC3uj8GEDOLfzGFHtmKIz_Us9", message_title="Changed", message_body=__name)
+app.config['SECRET_KEY'] = 'mysecret'
+socketio = SocketIO(app, cors_allowed_origins='*')
+
+@socketio.on('message')
+def handleMessage(msg):
+	print('Message: ' + str(msg))
+	send(msg, broadcast=True)
 
 
 @app.route('/')
-def index():
-    my_stream = db.child("stream").stream(stream_handler,user['idToken'])
-    return render_template('index.html')
-
+def start():
+    	return render_template('index.html')
