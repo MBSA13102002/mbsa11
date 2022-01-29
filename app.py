fbd5b30bef2f47e72bae2bce213e2f3aba1586f1
MBSA13102002 <73106667+MBSA13102002@users.noreply.github.com>
2022-01-29 21:13:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
-from flask import Flask ,render_template
-from flask_socketio import SocketIO, send,emit
-
+from flask import Flask,render_template
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecret'
-socketio = SocketIO(app, cors_allowed_origins='*')
-
-@socketio.on('message')
-def handleMessage(msg):
-	print('Message: ' + str(msg))
-	send(msg, broadcast=True)
 
 
-# @app.route('/')
-# def start():
-#     	return render_template('index.html')
-
+@app.route('/',methods = ['GET','POST'])
+def start():
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.run(debug =True)
